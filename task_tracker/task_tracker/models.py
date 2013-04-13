a2caa46f97f7eacd6d8165cec4204f58f12c1fb0
strazdas <strazdas@tormail.org>
2013-04-13 17:40:50 +0300

    Added Task model.

--- task_tracker/task_tracker/models.py
+++ task_tracker/task_tracker/models.py
@@ -8,21 +8,12 @@ DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text, unique=True)
-    value = Column(Integer)
-
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-
-
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     stories = relationship("Story", backref="created_by")
+    created_tasks = relationship("Task", backref="created_by")
+    assigned_tasks = relationship("Task", backref="assigned")
 
 
 class Story(Base):
@@ -33,3 +24,16 @@ class Story(Base):
     estimated = Column(Text, nullable=True)
     created = Column(DateTime)
     created_by_id = Column(Integer, ForeignKey('user.id'), nullable=True)
+    tasks = relationship('Task', backref="story")
+
+
+class Task(Base):
+    __tablename__ = 'task'
+    id = Column(Integer, primary_key=True)
+    title = Column(Text)
+    description = Column(Text, nullable=True)
+    estimated = Column(Text, nullable=True)
+    created = Column(DateTime)
+    assigned_id = Column(Integer, ForeignKey('user.id'), nullable=True)
+    created_by_id = Column(Integer, ForeignKey('user.id'), nullable=True)
+    story_id = Column(Integer, ForeignKey('story.id'))
