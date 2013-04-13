09230123fe6a23a3d540d3a555e2f9eb77fb1ad9
strazdas <strazdas@tormail.org>
2013-04-13 09:26:50 +0300

    Added description field for Story model.

--- task_tracker/task_tracker/models.py
+++ task_tracker/task_tracker/models.py
@@ -28,7 +28,8 @@ class User(Base):
 class Story(Base):
     __tablename__ = 'story'
     id = Column(Integer, primary_key=True)
-    title = Column(Text)  # TODO: use unique=True and create form validator
+    title = Column(Text)
+    description = Column(Text, nullable=True)
     estimated = Column(Text, nullable=True)
-    created = Column(DateTime, nullable=True)
+    created = Column(DateTime)
     created_by_id = Column(Integer, ForeignKey('user.id'), nullable=True)
